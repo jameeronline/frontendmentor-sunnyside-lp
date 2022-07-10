4dd5d281272abe499602b20298c912a4ea2a72ee
Jamal Mohamed Ameer <jamalbe2004@gmail.com>
2022-07-10 22:45:42 +0300

    responsive and mobile menu implemented

--- js/script.js
+++ js/script.js
@@ -2,5 +2,10 @@ document.getElementById('hamburger-menu').onclick = function(){
     var element = document.getElementsByClassName('main-menu')[0];
     var classList = element.classList;
     classList.toggle('show');
+
+    var mobileMenu = document.getElementsByClassName('hamburger-menu')[0];
+    var mobileMenuClasses = mobileMenu.classList;
+    mobileMenuClasses.toggle('active');
+
     return false;
 };
